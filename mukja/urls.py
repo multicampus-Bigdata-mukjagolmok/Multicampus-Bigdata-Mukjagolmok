f9946e1b647d1b487a5172db842c24db82e1ac74
Kim Jae Eun <kj6659@naver.com>
2021-02-08 20:46:48 +0900

    1

--- mukja/urls.py
+++ mukja/urls.py
@@ -4,4 +4,5 @@ from . import views
 # app_name = 'mukja'	# 이름 공간
 urlpatterns = [
     path('', views.base, name = 'base'),
+    path('golmok1/', views.golmok1, name='golmok1')
 ]
--- mukja/views.py
+++ mukja/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from mukja.models import Golmok, Restaurant, Menu
 
 # Create your views here.
 
 def base(request):
     return render(request, 'base.html')
+
+def golmok1(request):
+    menu = Menu.objects.all()
+    context = {"menu": menu}
+    return render(request, 'golmok1.html', context)
